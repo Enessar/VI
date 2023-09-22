88c0cd2200e44ebfbea76e94cbb3727209bb0c84
Rassene M'sadaa <rassens.msadaa@gmail.com>
2023-09-22 14:05:29 +0100

    contry pop finished

--- Our porject/src/pre-processing.py
+++ Our porject/src/pre-processing.py
@@ -3,4 +3,5 @@ import pandas as pd
 # Cleaning Country_population
 pop_df = pd.read_csv("Our porject\src\Initial_data\country_population.csv", encoding='latin1', delimiter=',')
 pop_df.drop(["Indicator Name","Indicator Code"], axis=1, inplace=True)
+pop_df.rename(columns={'ï»¿"Country Name"': 'Country name'}, inplace=True)
 pop_df.to_csv('Our porject\src\country_pop.csv', index=False, header=True)
