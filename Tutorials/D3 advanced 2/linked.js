ffe5740aee0ab9a87172e0c65d2fc3ba7e22baf0
Rassene M'sadaa <rassens.msadaa@gmail.com>
2023-10-11 13:56:28 +0100

    in progress

--- Tutorials/D3 advanced 2/linked.js
+++ Tutorials/D3 advanced 2/linked.js
@@ -14,6 +14,32 @@ function handleMouseOver(event, item) {
       }
     })
     .attr("fill", "red"); // Change the fill color of the matching elements to red
+
+  // Create and display a tooltip with country information
+  const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("opacity", 0);
+
+    // var coordinates= d3.pointer(this);
+    // var mouseX = coordinates[0];
+    // var mouseY = coordinates[1];
+
+  // Populate the tooltip with information
+  tooltip.html(
+    `<strong>Country:</strong> ${item.properties.name}<br>
+     <strong>Income Per Person:</strong> ${item.properties.incomeperperson}<br>
+     <strong>Alcohol Consumption:</strong> ${item.properties.alcconsumption}<br>
+     <strong>Employment Rate:</strong> ${item.properties.employrate}`
+  )
+  .style("background-color", "rgba(128, 128, 128, 0.7)") // Grey with 70% transparency
+  .style("padding", "8px") // Adjust the padding as needed
+  .style("border", "1px solid #333") // Optional border
+  .style("left", (event.pageX + 10) + "px")
+  .style("top", (event.pageY - 20) + "px")
+  .transition()
+  .duration(200)
+  .style("opacity", 0.9);
 }
 
 // Function to handle mouseout event
@@ -43,6 +69,16 @@ function handleMouseOut(event, item) {
       })
       .attr("fill", d3.interpolateBlues(colorScale(element.incomeperperson)));
   });
+
+  // Remove the tooltip
+  d3.select(".tooltip")
+    .transition()
+    .duration(200)
+    .style("opacity", 0);
+
+    // Remove the tooltip div
+  d3.select(".tooltip").remove();
+
 
   // Reset the fill color of all elements with class "circle data" to steelblue
   d3.selectAll("circle.data").attr("fill", "steelblue");
