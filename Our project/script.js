f9d3b0eaa9b792f534dbdad613298a5a6fbb731d
Rassene M'sadaa <rassens.msadaa@gmail.com>
2023-10-13 18:46:52 +0100

    beginning the legend of the map not finished

--- Our project/script.js
+++ Our project/script.js
@@ -9,8 +9,8 @@ const margin = {
     bottom: 50,
     left: 80,
   };
-  const width = 500 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
+  const width = 900 - margin.left - margin.right;
+  const height = 600 - margin.top - margin.bottom;
 
 
 function startDashboard(){
@@ -78,20 +78,20 @@ function createChoroplethMap() {
   
     // Create a color scale for the incomeperperson values
     const colorScale1 = d3
-    .scaleLinear()
+    .scaleLog()
     .domain([
     d3.min(globalData, (d) => d.life_expectancy),
     d3.max(globalData, (d) => d.life_expectancy),
     ])
-    .range([0, 1]);
+    .range([0,1]);
 
     const colorScale2 = d3
-    .scaleLinear()
+    .scaleLog()
     .domain([
     d3.min(globalData, (d) => d.Fertility_Rate),
     d3.max(globalData, (d) => d.Fertility_Rate),
     ])
-    .range([0, 1]);
+    .range([0,1]);
   
     // Create a projection to convert geo-coordinates to pixel values
     const projection = d3
@@ -109,8 +109,8 @@ function createChoroplethMap() {
       .append("path")
       .attr("class", "country data")
       .attr("d", path)
-    //   .attr("stroke", "black")
-    //   .attr("stroke-width", 0.5) // Adjust this value to make the stroke very thin
+      .attr("stroke", "black")
+      .attr("stroke-width", 0.1) // Adjust this value to make the stroke very thin
     //   .on("mouseover", handleMouseOver) // Function to handle mouseover event
     //   .on("mouseout", handleMouseOut)   // Function to handle mouseout event
     //   .on("click", handleMouseClick)
@@ -122,26 +122,29 @@ function createChoroplethMap() {
       mapGroup
         .selectAll("path")
         .filter(function (d) {
-          return d.properties.name == element.Country_name;
+          return d.properties.name == element.Country_name && element.Year == 2016;
         })
         .attr("fill", 
-        d3.interpolateBlues(colorScale1(element.life_expectancy))
+        d3.interpolate(
+            d3.interpolateGreens(colorScale1(element.life_expectancy))
+            ,
+            d3.interpolateReds(colorScale2(element.Fertility_Rate))
+                )(0.5)
         // (d) => {
-        //     const value1 = d.life_expectancy; // Replace with your actual dimension1 value
-        //     const value2 = d.Fertility_Rate; // Replace with your actual dimension2 value
+        //     const value1 = element.life_expectancy; // Replace with your actual dimension1 value
+        //     const value2 = element.Fertility_Rate; // Replace with your actual dimension2 value
         
 
         //     // Calculate the relative weight of each dimension in color mixing
         //     const weight1 = value1 / (value1 + value2);
         //     const weight2 = value2 / (value1 + value2);
 
-        //     // Interpolate colors for both dimensions and combine them
-        //     const color1 = d3.interpolateBlues(colorScale1(element.life_expectancy));
-        //     const color2 = cd3.interpolateBlues(colorScale1(element.Fertility_Rate));
+        //     const color1 = d3.interpolateBlues(weight1); // Use interpolateBlues for the first dimension
+        //     const color2 = d3.interpolateOranges(weight2); // Use interpolateOranges for the second dimension
+      
+        //     // Calculate the mixed color
+        //     return d3.interpolate(color1, color2)(0.5); // You can adjust the weight as needed
 
-        //     const mixedColor = d3.rgb(color1).mix(d3.rgb(color2), weight2);
-
-        //     return mixedColor;
         // }
         );
     });
@@ -149,7 +152,7 @@ function createChoroplethMap() {
     // Create zoom behavior for the map
     const zoom = d3
       .zoom()
-      .scaleExtent([1, 10])
+      .scaleExtent([1, 15])
       .translateExtent([
         [0, 0],
         [width, height],
@@ -164,50 +167,55 @@ function createChoroplethMap() {
       mapGroup.attr("transform", event.transform);
     }
   
-    // // Create a legend for the choropleth map
-    // const svg2 = d3
-    //   .select("#choroplethLabel")
-    //   .append("svg")
-    //   .attr("width", width * 0.2)
-    //   .attr("height", height);
-  
-    // // Create a gradient for the legend color scale
-    // const defs = svg2.append("defs");
-    // const gradient = defs
-    //   .append("linearGradient")
-    //   .attr("id", "colorScaleGradient")
-    //   .attr("x1", "0%")
-    //   .attr("y1", "0%")
-    //   .attr("x2", "0%")
-    //   .attr("y2", "100%");
-  
-    // gradient
-    //   .append("stop")
-    //   .attr("offset", "0%")
-    //   .attr("stop-color", d3.interpolateBlues(0));
-  
-    // gradient
-    //   .append("stop")
-    //   .attr("offset", "100%")
-    //   .attr("stop-color", d3.interpolateBlues(1));
-  
-    // // Create the legend rectangle filled with the color scale gradient
-    // const legend = svg2.append("g").attr("transform", `translate(0, 40)`);
-    // const legendHeight = height - 40;
-    // const legendWidth = 20;
-  
-    // legend
-    //   .append("rect")
-    //   .attr("width", legendWidth)
-    //   .attr("height", legendHeight)
-    //   .style("fill", "url(#colorScaleGradient)");
-  
-    // // Add tick marks and labels to the legend
-    // for (let index = 0; index <= 1; index += 0.25) {
-    //   legend
-    //     .append("text")
-    //     .attr("x", legendWidth + 5)
-    //     .attr("y", legendHeight * index)
-    //     .text(Math.round(colorScale.invert(index)));
-    // }
+    // Create a legend for the choropleth map
+    const legendWidth = 250; // Width of the legend
+    const legendHeight = 250; // Height of the legend
+    
+    const legendSvg = d3.select("#choroplethLabel")
+        .append("svg")
+        .attr("width", legendWidth )
+        .attr("height", legendHeight);
+        
+    const heatWidth = 200;
+    const heatHeight = 200;
+
+    const numColumns = 10; // Number of columns (dimension 1)
+    const numRows = 10; // Number of rows (dimension 2)
+    
+    const columnWidth = heatWidth / numColumns;
+    const rowHeight = heatHeight / numRows;
+
+
+    
+    for (let i = 0; i < numColumns; i++) {
+        for (let j = 0; j < numRows; j++) {
+            const mixedColor = d3.interpolate(
+                d3.interpolateGreens((i + 0.5) / numColumns),
+                            d3.interpolateReds((j + 0.5) / numRows))(0.5) // Adjust the mixing ratio as needed
+            
+    
+            legendSvg.append("rect")
+                .attr("x", i * columnWidth +50)
+                .attr("y", heatHeight -j * rowHeight)
+                .attr("width", columnWidth)
+                .attr("height", rowHeight)
+                .style("fill", mixedColor);
+        }
+    }
+
+    const legend = legendSvg.append("g").attr("transform", `translate(0, 0)`);
+
+
+    // Add tick marks and labels to the legend
+    for (let index = 0; index <= 1; index+=0.2) {
+        // console.log(colorScale1.invert(index))
+        legend
+        .append("text")
+        .attr("x", 0)
+        .attr("y", legendHeight - legendHeight * index + 10)
+        .text(Math.round(colorScale2.invert(index)));
+    }
+
+    // Position the legend on the page
+    legendSvg.attr("transform", "translate(10, 20)"); // Adjust the translation as needed
   }
